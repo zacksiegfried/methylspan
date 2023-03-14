494b502a3ca60135c65702532f1c7ff1686297ef
Zack Siegfried <102556973+zacksiegfried@users.noreply.github.com>
2023-03-14 11:57:30 -0400

    initial commit

---
+++ MSM.py
@@ -0,0 +1,127 @@
+import MethylDataFetch
+import os
+import random
+import pandas as pd
+
+from functools import reduce
+
+"""Contains functions related to formatting metadata and methylation beta arrays"""
+
+
+def pullMethylMetaData(primary_site):
+    """Returns list of dictionaries with metadata matching primary site cases that are dead with days to death data"""
+
+    full_file_info = MethylDataFetch.getMethylMetaData(str(primary_site))
+
+    ### could build into pull "Dead", pull "Alive" functions if useful
+    #if file["cases"][0]["demographic"]["vital_status"] == str(vital_status):
+        #if file["cases"][0]["demographic"]["days_to_death"] != None:
+    
+    print("Number of methylation files for all cases for primary site " + str(primary_site) + ": " + str(len(full_file_info)))
+    return(full_file_info)
+
+# Used for finding the 'beta_arrays' folder where methylation arrays are stored 
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+def methylDataFormat(all_files, sample_size = None):
+    """Returns methylation array as a df. Index column is file_is, column headers are CpG site #"""
+
+    # Uses all the data by default, allows reduction of cases to speed up output
+    if sample_size == None:
+        sample_set = all_files
+    else:
+        sample_set = random.sample(all_files, sample_size)
+
+    # find sub directory name
+    for file in os.listdir(str(script_dir + "/beta_arrays")):
+        d = os.path.join(str(script_dir + "/beta_arrays"), file)
+        if os.path.isdir(d):
+            sub_folder = d
+
+
+    # makes list of dataframes that match the file_names in sample_set (sluggish)
+    sample_df_list = []
+    # iterates over list of metadata dictonaires that have been randomly selected (=sample_no = 100)
+    for entry in sample_set:
+        # iterates over the files inside the sub directory of beta_arrays (should all be directories with generated names)
+        for file in os.listdir(str(sub_folder)):
+            d = os.path.join(str(sub_folder), file)
+            if os.path.isdir(d): # if file is directory
+                # for file in sub sub directory, if file name == file name in training set metadata add to df list
+                for data in os.listdir(str(d)):
+                    if str(data) == entry['file_name']:
+                        file_path = str(d + "/" + data)
+                        df = pd.read_csv(file_path, sep="\t", header=None)
+                        df.columns = ['file_id', entry['file_id']]
+                        sample_df_list.append(df)
+        
+
+
+    # merging methylation datasets, dropping cases with lots of missing values and CpG sites with no reads
+    sample_df = reduce(lambda x, y: pd.merge(x, y, on = 'file_id'), sample_df_list)
+    sample_df = sample_df.transpose()
+    sample_df.columns = sample_df.iloc[0]
+    sample_df = sample_df.iloc[1:]
+    sample_df = sample_df.dropna(thresh=300000)
+    sample_df = sample_df.dropna(axis=1, thresh=5)
+    sample_df = sample_df.astype(float)
+
+    return(sample_df)
+
+
+def metaDataFormat(df, all_files):
+    """Returns meta data as df. Index column is file_id, metadata = days_to_death/days_in_study, vital_status"""
+    # makes list of days to death dataframes matching file_ids with beta arrays passing missing data conditions
+    index = list(df.index)
+    meta_df_list = []
+    for entry in all_files:
+        if entry['file_id'] in index:
+            # Alive cases handling
+            if entry['cases'][0]['demographic']['vital_status'] == "Alive":
+                try:
+                    meta_df_list.append(
+                        {                                
+                            'file_id': entry['file_id'],
+                            'days_to_death': entry['cases'][0]['diagnoses'][0]['days_to_last_follow_up'],
+                            'vital_status': entry['cases'][0]['demographic']['vital_status']
+                        }
+                    )
+                except KeyError:
+                    meta_df_list.append(
+                        {
+                            'file_id': entry['file_id'],
+                            'days_to_death': None,
+                            'vital_status': entry['cases'][0]['demographic']['vital_status']
+                        }
+                    )
+            # Dead cases handling 
+            elif entry['cases'][0]['demographic']['vital_status'] == "Dead":
+                try:
+                    meta_df_list.append(
+                        {
+                            'file_id': entry['file_id'],
+                            'days_to_death': entry['cases'][0]['demographic']['days_to_death'],
+                            'vital_status': entry['cases'][0]['demographic']['vital_status']
+                        }
+                    )
+                except KeyError:
+                    meta_df_list.append(
+                        {
+                            'file_id': entry['file_id'],
+                            'days_to_death': None,
+                            'vital_status': entry['cases'][0]['demographic']['vital_status']
+                        }
+                    )
+
+    # merging meta datasets and formatting for use in glm
+    meta_df = pd.DataFrame(meta_df_list)
+    meta_df = meta_df.set_index(list(meta_df)[0])
+    meta_df.index.name = None
+    meta_df = meta_df.squeeze()
+
+    # prints final number of sets that pass missing data conditions
+    shape = meta_df.shape
+    print('Final number of cases in sample dataset :', shape[0])
+
+    return(meta_df)
+
---
+++ MethylDataFetch.py
@@ -0,0 +1,132 @@
+import requests
+import json
+import re
+import os
+
+"""Contains functions related to pulling metadata and downloading methylation beta arrays"""
+
+
+def getMethylMetaData(primary_site, file_type="Methylation Beta Value"):
+    """Returns list of dictonaries of specfic meta data fields corresponding to input priamry site"""
+    # filters to return on files with methylation data from chosen primary site
+    filters = {
+        "op": "and",
+        "content":[
+            {
+            "op": "in",
+            "content":{
+                "field": "cases.primary_site",
+                "value": [str(primary_site)]
+                }
+            },
+            # methylation data filter
+            {
+            "op": "in",
+            "content":{
+                "field": "files.data_category",
+                "value": ["dna methylation"]
+                }
+            },
+            {
+            "op": "in",
+            "content":{
+                "field": "files.data_type",
+
+                ### "Masked Intensities" selects for .IDAT raw methylation files
+                ### "Methylation Beta Values" selects for .txt beta arrays files
+                "value": [str(file_type)]
+                }
+            }
+        ]
+    }
+
+    # meta data fields requested
+    ### could add more data catagories here (bmi, sex... ) https://docs.gdc.cancer.gov/API/Users_Guide/Appendix_A_Available_Fields/#file-fields
+    fields = [
+        "file_id",
+        "file_name",
+        "file_size",
+        "platform",
+
+        "cases.case_id",
+
+        "cases.diagnoses.created_datetime",
+        "cases.diagnoses.days_to_last_follow_up",
+
+        "cases.demographic.vital_status",
+        "cases.demographic.days_to_birth",
+        "cases.demographic.days_to_death",
+
+        "cases.samples.sample_id",
+        "cases.samples.created_datetime"
+        ]
+    fields = ",".join(fields)
+
+    params = {
+        "filters": json.dumps(filters),
+        "fields": fields,
+        "format": "JSON",
+        "size": "1000"
+        }
+
+    response = requests.get("https://api.gdc.cancer.gov/files", params = params)
+
+    data_array = json.loads(response.content.decode("utf-8"))["data"]["hits"]
+    return data_array
+
+
+def checkDownloadSize(primary_site, file_type="Methylation Beta Value"):
+    """Prints size of all methylation files in filtered search"""
+    size = 0
+    counter = 0 
+    for file in getMethylMetaData(str(primary_site), str(file_type)):
+        size += file["file_size"]
+        counter += 1
+
+    print("Number of matching files = " + str(counter))
+    print("File download size is " + str((size/1000000)) + " megabytes")
+    print(str((size/1000000000)) + " gigabytes")
+
+
+def downloadMethylData(primary_site, file_type="Methylation Beta Value"):
+    """Downloads methylation files of requested type and primary site into folder beta_arrays"""
+
+    ### needs testing
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    try:
+        os.mkdir(script_dir + "/beta_arrays")
+    except OSError as error:
+        print(error)
+
+
+    checkDownloadSize(primary_site, file_type)
+    proceed = input("Would you like to download? (Y/N): ")
+
+    if proceed != "Y":
+        return
+    else:
+
+        uuid_list = []
+        for file in getMethylMetaData(str(primary_site), str(file_type)):
+            uuid_list.append(file['id'])
+
+        params = {"ids": uuid_list}
+
+        response = requests.post("https://api.gdc.cancer.gov/data",
+                                data = json.dumps(params),
+                                headers={
+                                    "Content-Type": "application/json"
+                                        })
+
+        response_head_cd = response.headers["Content-Disposition"]
+        file_name = re.findall("filename=(.+)", response_head_cd)[0]
+
+        save_name = script_dir + "/beta_arrays/" + file_name
+        with open(save_name, "wb") as output_file:
+            output_file.write(response.content)
+
+
+    
+
+### downloading all methylation beta arrays for pancreas
+#downloadMethylData('pancreas')
