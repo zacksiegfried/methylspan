d0be016486cb55c9e5c1d9a71b6576c13b48befc
Zack Siegfried <102556973+zacksiegfried@users.noreply.github.com>
2023-05-26 11:13:47 -0400

    dropping missing genes + average all genes

--- MethylMapping.py
+++ MethylMapping.py
@@ -7,8 +7,9 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 
 ### Could add platform specific mapping either by buidling other functions and subsetting sample data set
 # or by tagging sample data set with platform type (not sure if this makes programatic sense)
-def methylMap450(methylation_beta_array):
-    """Transforms column names from IlmnID to UCSC_RefGene_Name using 450 manifest"""
+def methylMap450(methylation_beta_array, drop_missing = 'Y'):
+    """Transforms column names from IlmnID to UCSC_RefGene_Name using 450 manifest,
+        drops CpG sites with no mapping information by default"""
 
     # sets files directory as working directory
     os.chdir(script_dir)
@@ -20,14 +21,16 @@ def methylMap450(methylation_beta_array):
     
     methylation_beta_array.rename(columns=mapping_dict, inplace=True)
 
+    if drop_missing == 'Y':
+        methylation_beta_array = methylation_beta_array.drop([""], axis=1)
+
     return(methylation_beta_array)
 
 
-def averageGene(mapped_sample_data, gene):
-    """Averages mapped methylation beta array beta values from specified gene (not scientific)"""
+def averageGene(mapped_sample_data):
+    """Averages mapped methylation beta array beta values for all similar genes (not scientific)"""
 
     df = mapped_sample_data.groupby(by=mapped_sample_data.columns, axis=1).mean()
-    df = pd.DataFrame(df[gene])
     print(df.head())
 
     return(df)
